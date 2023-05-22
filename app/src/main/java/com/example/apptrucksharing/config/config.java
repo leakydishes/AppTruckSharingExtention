c198073aca5312e9de4ec871da71dc9a8478c644
teclaire <teclairestudios@gmail.com>
2023-05-22 16:51:38 +1000

    Updated files

--- app/src/main/java/com/example/apptrucksharing/config/config.java
+++
@@ -1,6 +0,0 @@
-package com.example.apptrucksharing.config;
-
-public class config {
-
-    public static final String PAYPAL_CLIENT_ID="AeBqHRAZBrBrYaw-ZaFaeTdBacYICCBwn03JGqDqDVX9ZDo5a6Sx8AjPfcwIEyBUY5obQMkTwPYi6moQ";
-}
